5c59fed36b00e7e17b00f8de1f7183db26e26299
saronraut <47161225+saronraut@users.noreply.github.com>
2020-12-15 14:19:50 -0600

    inserting table into the page

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,19 @@
 // from data.js
-var tableData = data;
+// var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+let table = d3.select("")
+
+// inserting table into the the page
+tableData.forEach(item => {
+    console.log(item)
+    let row = tbody.append("tr");
+    let date = row.append("td");
+    let city = row.append("td");
+    let state = row.append("td");
+    let country = row.append("td");
+    let shape = row.append("td");
+    let duration = row.append("td");
+    let comments = row.append("td");
+    date.text(item.datetime);
+})
